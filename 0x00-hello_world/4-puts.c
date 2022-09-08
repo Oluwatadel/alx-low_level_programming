d796af084ff3af31af9f9861fe8749f9cfaed01b
Oluwatobi <Oluwatobiadelesi@gmail.com>
2022-09-08 18:25:41 +0000

    using put to display

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * This program is to output Programming is like building a
+ * multilingual puzzle
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

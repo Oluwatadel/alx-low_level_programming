4837eae654e6b58ef85b67b0ef9d6d6cca5fad07
Oluwatobi <Oluwatobiadelesi@gmail.com>
2022-09-09 21:17:28 +0000

    me

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -20,7 +20,6 @@ int main(void)
 
 		a++;
 	}
-	
 	putchar('\n');
 
 
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-9 using putchar
+ *
+ * Return: Always 0 (succcess)
+ */
+
+int main(void)
+{
+	int num;
+	for (num = 0; n < 10; num++)
+		putchar(num);
+
+
+putchar ('\n');
+
+return (0)
+}

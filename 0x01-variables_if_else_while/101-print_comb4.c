5b46a7f85f083cf6e4b2f5a0e4845b75255374e1
Oluwatobi <Oluwatobiadelesi@gmail.com>
2022-09-09 22:53:38 +0000

    all done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,31 +8,30 @@
  */
 int main(void)
 {
-	int digit1, digit2;
+	int digit1, digit2, digit3;
 
-	for (digit1 = 10; digit1 < 19; digit1++)
-
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		if ((digit2 % 10) < (digit1 % 10))
-
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			putchar((digit1 % 10) + '0');
-			
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 != 18 || digit2 != 19)
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-			putchar(',');
+				putchar((digit1 % 10) + '0');
+				
+				putchar((digit2 % 10) + '0');
+
+				putchar((digit3 % 10) + '0');
 			
+			if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+			
+				continue;
+			
+			putchar(',');
 			putchar(' ');
 			}
-
 		}
 	}
-
-
-	putchar('\n');
-
-
-	return (0);
-}
+		putchar('\n');
+		
+		return (0);
+	}

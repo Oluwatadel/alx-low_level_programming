14154b414906d53553093d3900a2b97a7a83f929
Oluwatobi <Oluwatobiadelesi@gmail.com>
2022-09-09 15:22:35 +0000

    go

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,13 +18,13 @@ int main(void)
 	if (n < 0)
 	{
 		printf("is positive %d\n", n);
-	}
-	else if (n == 0)
+	}else if (n > 0)
 	{
 		printf("is zero %d\n", n);
 	}
 	else
 	{
 		printf("is negative %d\n", n);
+	}
 	return (0);
 }

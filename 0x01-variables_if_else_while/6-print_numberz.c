4ad55adb742d05be4c6b36468554f37136b1292d
Oluwatobi <Oluwatobiadelesi@gmail.com>
2022-09-09 21:34:33 +0000

    reverse

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int num;
+
 	num = '0';
 
 	while (num <= '9')
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	int alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha)
+		alpha--
+	}
+	putchar('\n');
+
+	return (0);
+}
+

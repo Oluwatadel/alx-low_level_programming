b77dbab667eb2b0925503f196794aca160780110
Oluwatobi <Oluwatobiadelesi@gmail.com>
2022-09-13 13:30:31 +0000

    printing all lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - writes the character c to stdout
+ *
+ * Description: To printed out because of the _putchar function
+ *
+ * Return: On success 0.
+ */
+int main(void)
+{
+	void print_alphabet(void);
+	{
+		char i = 'a';
+		while (i <= 'z')
+			_putchar('i');
+		_putchar('\n');
+		i++;
+	}
+	return (0);
+}

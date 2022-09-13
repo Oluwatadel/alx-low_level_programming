6aeb94bed64559297cb41417c4025b219bb9ea05
Oluwatobi <Oluwatobiadelesi@gmail.com>
2022-09-13 11:50:06 +0000

    getting deepr into c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - writes the character c to stdout
+ * Description: To printed out because of the _putchar function
+ * Return: On success 0.
+ */
+int main(void)
+{
+	putchar();
+
+	return (0);
+}

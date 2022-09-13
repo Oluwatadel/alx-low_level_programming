e831bc5b52fc968b3a4a77ace1aa1c6d536e124f
Oluwatobi <Oluwatobiadelesi@gmail.com>
2022-09-13 22:10:34 +0000

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,19 +8,20 @@
  *
  * Jack Bauer, starting from 00:00 to 23:59
  */
-jack_bauer(int hour, int minute)
+void jack_bauer(void)
+{
+		int hour, minute;
 
-
-	for (hour = 0; hour <= 23; hour++;)
-	{
-		for (minute = 0; minute <= 59; minute++;)
+		for (hour = 0; hour <= 23; hour++;)
 		{
-			_putchar(hour / 10 + '0');
-			_putchar(hour % 10 + '0');
-			_putchar(':');
-			_putchar(minute / 10 + '0');
-			_putchar(minute % 10 + '0');
-			_putchar('\n');
+			for (minute = 0; minute <= 59; minute++;)
+			{
+				_putchar(hour / 10 + '0');
+				_putchar(hour % 10 + '0');
+				_putchar(':');
+				_putchar(minute / 10 + '0');
+				_putchar(minute % 10 + '0');
+				_putchar('\n');
+			}
 		}
-	}
 }

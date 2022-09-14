35708b55808c0779505a558e9cc24a25056ca10c
Oluwatadel <oluwatobiadelesi>
2022-09-14 09:59:36 +0000

    done

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,15 +9,16 @@
  */
 int main(void)
 {
-	int i, sum = 0;
+	int i;
+	int sum = 0;
 
 	for  (i = 0; i < 1024; i++)
 
 	{
 		if ((i % 3) == 0 || (i % 5 == 0))
-		
+
 		{
-			sum += i
+			sum += i;
 		}
 
 		printf("%d\n", sum);

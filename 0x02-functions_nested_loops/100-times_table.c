3236924dfafeec638c4ef42ba81d0ad5843eb23e
Oluwatadel <oluwatobiadelesi>
2022-09-14 09:27:40 +0000

    error fixed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,9 +8,10 @@
 void print_times_table(int n)
 {
 	int a, b, c;
-	
 	if (n >= 0 && n <= 15)
+
 	{
+
 		for (a = 0; a <= n; a++)
 		{
 			for (b = 0; b <= n; b++)
@@ -20,14 +21,13 @@ void print_times_table(int n)
 				{
 					_putchar(c + '0');
 				} else if (c < 10 && b != 0)
-				
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(c + '0');
-				}else if (c >= 10 && c < 100)
+				} else if (c >= 10 && c < 100)
 				{
 					_putchar(',');
 					_putchar(' ');

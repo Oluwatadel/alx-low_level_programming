b7e882ed3fa9a853d6c60f5bc6699af1dcfec606
Oluwatadel <oluwatobiadelesi>
2022-09-15 17:06:57 +0000

    on the way

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * _isdigit -To check for a digit
+ * _isdigit - To check for a digit
  * @c: c to be check if it is a digit
  *
+ * Return: always 0
  */
 int _isdigit(int c)
 {
-	if(c >= 48 && c <= 57) 
+	if (c >= 48 && c <= 57)
 
 	{
 		return (1);
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - To print numbers more than one time
+ *
+ * @
+ *
+ * Return: always 0
+ *
+ */
+void more_numbers(void)
+{
+	int i, j;
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (i > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}

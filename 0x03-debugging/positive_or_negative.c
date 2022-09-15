da3f235eda2533fa555a18d64e1b4c56534db6ab
Oluwatadel <oluwatobiadelesi>
2022-09-15 07:49:48 +0000

    positive number debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * positive_or_negative - To check if 98 is postive or negative
+ * @i:	i is the number to check
+ *
+ * return: always 0
+ */
+
+void positive_or_negative(int i)
+{
+
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is positive\n", i);
+	}
+
+	return;
+}

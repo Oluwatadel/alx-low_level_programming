2ea0b5eca2c04f1fcf9038fbb1f9d4a27fdb2a0c
Oluwatadel <oluwatobiadelesi>
2022-09-15 12:26:38 +0000

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,28 +9,18 @@
 
 void print_to_98(int n)
 {
-	int i, j;
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i != 98)
-				printf("%d, ", i);
-
-			else if (i == 98)
-				printf("%d\n", n);
-		}
+		while (n > 98)
+			printf("%d" , n--);
+		printf("%d\n", n);
 	}
-
-	else if (n >= 98)
+	else
 	{
-		for (j = n; j >= 98; j--)
-		{
-			if (j != 98)
-				printf("%d, ", n--);
-			else if (j == 98)
-				printf("%d\n", j);
-		}
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
+

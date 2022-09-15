432e9b2b541f3d2a074b25da1b2d53b2e2017a96
Oluwatadel <oluwatobiadelesi>
2022-09-15 14:47:20 +0000

    done

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,14 +3,17 @@
 /**
  * mul - to multiply two integers
  * @a: first integer
- * @b: second integer
- * return: always 0
+ * @b: second intege
+ *
+ * Return: always 0
  */
 
 int mul(int a, int b)
 {
 	int c;
+
 	c = a * b;
-	return c;
+
+	return (c);
 }
 

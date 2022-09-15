8bf50680a3dbfe6f0b1e4f3b5f6d941a37efc774
Oluwatadel <oluwatobiadelesi>
2022-09-15 10:09:07 +0000

    largest number

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,15 +10,19 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
+/*
+ * There should be an increment for i which will make reference
+ * to i less than 10 in the while statement
+ */
 	while (i < 10)
+
 	{
 		putchar(i);
-/* 
- * There should be an increment for i which will make reference
- * to i less than 10
- */
 	}
+
 	printf("Infinite loop avoided! \\o/\n");
+	
 	return (0);
 }

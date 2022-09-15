297889cca3fbf5b7e194e33e9d596c16cf083ff3
Oluwatadel <oluwatobiadelesi>
2022-09-15 14:58:04 +0000

    one to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,14 @@
 #include "main.h"
+/**
+ * print_number - To print numbers from 0
+ * @i: charcter i
+ * Return: always 0
+ */
 
 void print_numbers(void)
 {
 	int i;
 	for (i = 48; i <= 57; i++)
+
 		_putchar(i);
 }

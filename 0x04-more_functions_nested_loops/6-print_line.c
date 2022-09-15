cdbdbfaae04729ee7ee9d265f14733af009e5654
Oluwatadel <oluwatobiadelesi>
2022-09-15 17:14:08 +0000

    on the way

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * print_line - To print a straight line
- * @i: the parameter for ieteration
+ * @n: the parameter for ieteration
  * Return: always 0
  */
 void print_line(int n)
 {
-	int i;
+	int n;
 
-	for (i = 0; i < n; i++)
+	for (n = 0; n < n; i++)
 	{
 		_putchar('_');
 	}

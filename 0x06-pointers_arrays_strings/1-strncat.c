601cbbc596cbc53e2090a2c11860de1cf98ee577
Oluwatadel <oluwatobiadelesi>
2022-09-21 20:53:04 +0000

    all done

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,9 +17,15 @@ char *_strncat(char *dest, char *src, int n)
 	i = 0;
 	j = 0;
 
-	while (dest[i++])
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
 		j++;
-	for (i = 0; src[i] && i < n; i++)
-		dest[i + j] = src[i];
+	}
+	dest[i] = '\0';
+
 	return (dest);
 }

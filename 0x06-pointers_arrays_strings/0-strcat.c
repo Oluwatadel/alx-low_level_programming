7701c881baa96f8e2e7b1deebbb2bbbddd7e7436
Oluwatadel <oluwatobiadelesi>
2022-09-21 20:22:00 +0000

    doing concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcat - To concatenate two strings
+ * @*dest: One of the character to concatenate
+ *
+ * Return: Always 0;
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (j = 0; dest[j] != '\0'; j++)
+	{}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i];
+	}
+
+	dest[j + i] = '\0';
+
+	return (dest);
+
+}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncat - To concatenate two strings using an inputted number of bytes
+ *
+ * @dest: The string to be appended
+ * @src: the second string to be appended
+ * @n: The number of time to concatenate
+ * Return: Always 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i++])
+		j++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[i + j] = src[i];
+	return (dest);
+}

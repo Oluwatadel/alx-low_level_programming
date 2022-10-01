bec8135553734ef51f7b02faf40b217ce8df5b00
Oluwatadel <oluwatobiadelesi>
2022-10-01 11:36:25 +0000

    done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,6 +17,7 @@ int main(int argc, char  *argv[])
 	if (argc == 2)
 	{
 		int curren;
+
 		if (strchr(argv[argc - 1], '-'))
 		{
 			printf("0\n");
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,13 +9,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int n, m;
-
-	n = atoi(argv[1]);
-	m = atoi(argv[2]);
+	int n = 0;
+    	int m = 0;
 
 	if (argc == 3)
 	{
+		n = atoi(argv[1]);
+		m = atoi(argv[2]);
+
 		printf("%d\n", n * m);
 		return (0);
 	}

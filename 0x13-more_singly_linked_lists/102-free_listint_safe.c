b6a684784e943b370132c24caf7f8d91ab0a9298
Oluwatadel <oluwatobiadelesi>
2022-10-25 17:07:45 +0000

    redo

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,42 +1,23 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * free_listint_safe - thsi function free a list in safe mode
- * @h: the head of list
- * Description: this function free a string in a safe mode
- * section header: the header of this function is lists.h)*
- * Return: the size of the list
+ * find_listint_loop - Find the loop inside a list
+ * @head: Pointer to the start of a list
+ *
+ * Return: Address of where the loop starts, NULL if
+ * no loop is found
  */
-
-size_t free_listint_safe(listint_t **h)
+listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *tmp, *actual;
-	size_t i;
-	int rest;
+	listint_t *holder;
 
-	i = 0, actual = *h;
-
-	while (actual)
+	while (head != NULL)
 	{
-		rest = actual - actual->next;
-		if (rest > 0)
-		{
-			tmp = actual->next;
-			free(actual);
-			actual = tmp;
-			i++;
-		} else
-		{
-			free(actual);
-			*h = NULL;
-			i++;
-			break;
-		}
-
+		holder = head;
+		head = head->next;
+		if (holder < head)
+			return (head);
 	}
-
-	*h = NULL;
-
-	return (i);
+	return (NULL);
 }
